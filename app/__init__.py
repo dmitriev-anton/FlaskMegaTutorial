c09569c12f4dea700e0e591438587d88c2b7684b
Anton Dmitriev <spaceboobman@gmail.com>
2024-07-27 20:57:44 +0400

    Flask+sqlaclhemy

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,12 @@
 from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 app = Flask(__name__) # тут app это переменная
+app.config.from_object(Config) # берем настройки из класса
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
 # нижний импорт известное решение, позволяющее избежать циклического импортаб , распространенной проблемы с приложениями Flask
-from app import routes # impo
+from app import routes, models   # impo
